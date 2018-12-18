fff314d942e50969a2f9f522d4a831d39cb33cd4
TrainerA@hexaware.com <TrainerA@hexaware.com>
2018-12-18 23:46:54 +0530

    JMockIt with JDBI Example Added

--- JMockitWithJDBI/src/main/java/com/persistence/DbConnection.java
+++ JMockitWithJDBI/src/main/java/com/persistence/DbConnection.java
@@ -1,10 +1,10 @@
-package com;
+package com.persistence;
 import org.skife.jdbi.v2.DBI;
 public class DbConnection {
 public final DBI getConnect() {
     	try {
       	Class.forName("com.mysql.jdbc.Driver");
-      	DBI dbi = new DBI("jdbc:mysql://localhost:3306/mydb", "root", "root");
+      	DBI dbi = new DBI("jdbc:mysql://localhost:3306/test", "root", "root");
       	return dbi;
     	} catch (ClassNotFoundException e) {
       	throw new RuntimeException(e);
--- JMockitWithJDBI/src/main/java/com/persistence/EmployeeDao.java
+++ JMockitWithJDBI/src/main/java/com/persistence/EmployeeDao.java
@@ -1,29 +1,28 @@
-package com;
+package com.persistence;
+import com.model.Employee;
 import java.util.*;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
-public interface EmpDao {
-	@SqlQuery("select * from emp")
-	@Mapper(EmpMapper.class)
-	public List<Emp> getAllEmployees();
+
+public interface EmployeeDao {
+	@SqlQuery("select * from employee")
+	@Mapper(EmployeeMapper.class)
+	public List<Employee> getAllEmployees();
 	
-	@SqlUpdate("insert into emp values(:id,:name,:salary)")
+	@SqlUpdate("insert into employee values(:id,:name,:salary)")
 	public int addEmpDao(@Bind("id") int id, @Bind("name") String name,@Bind("salary") float salary);
 
-	@SqlUpdate("delete from emp where id = :id")
+	@SqlUpdate("delete from employee where id = :id")
 	public int deleteEmpDao(@Bind("id") int id);
 
-	@SqlUpdate("update emp set salary = salary + :salary where id = :id")
+	@SqlUpdate("update employee set salary = salary + :salary where id = :id")
 	public int incrementEmpSalary(@Bind("id") int id, @Bind("salary") float salary);
 
 
-	@SqlUpdate("update emp set salary = salary - :salary where id = :id")
+	@SqlUpdate("update employee set salary = salary - :salary where id = :id")
 	public int decrementEmpSalary(@Bind("id") int id, @Bind("salary") float salary);
 
 
-	@SqlQuery("select * from emp where id = :id")
-	@Mapper(EmpMapper.class)	
-	public Emp displayOneEmp(@Bind("id") int id);		
 }
